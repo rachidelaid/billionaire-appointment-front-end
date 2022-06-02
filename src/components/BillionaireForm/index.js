99b4c743ab16b37dd025355312ca446e3f402edc
orozcoding <orozcoding@gmail.com>
2022-06-02 10:47:37 -0400

    Create new Billionaire component and views folders

---
+++ src/components/BillionaireForm/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './style.module.css';
+
+const BillionaireForm = () => (
+  <h1 className={`${style.blue} bold-text`}>Navbar</h1>
+);
+export default BillionaireForm;
