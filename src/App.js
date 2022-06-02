e3d475e2af5e8ed822f2393e966c8ad7698c9f5b
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-02 12:09:56 +0100

    Setup folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
