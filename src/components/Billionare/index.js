c8e5b1340ccd9f9f20adb06d1db397cd42e30043
matovu-farid <matovu90@gmail.com>
2022-06-03 00:30:50 +0300

    Add next button

--- src/components/Billionare/index.js
+++ src/components/Billionare/index.js
@@ -4,11 +4,10 @@ import style from './style.module.css'
 const Billionare = ({item}) => {
   return (
     <div className={style.container}>
-      <div className={style['bg-circle']}>
       <img src={item.image} alt={item.name} className={style.image}  />
       <h3 className={style.name}>{item.name}</h3>
       <h4 className={style.title}>{item.title}</h4>
-      </div>
+      
     </div>
   )
 }
