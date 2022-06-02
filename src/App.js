94c5a5cd754395a35621652992caa47a5775aac4
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-02 16:47:37 +0100

    Move Appointment folder to pages folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Appointment from './views/Appointment';
+import Appointment from './pages/Appointment';
 
 function App() {
   return (
