8c72181d127c1cc9d5cdd4727b36841e6b215288
orozcoding <orozcoding@gmail.com>
2022-06-03 04:37:24 -0400

    Create file structure

---
+++ src/components/BillionareDeleteList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './style.module.css';
+
+const BillionareDeleteList = () => (
+  <div>
+    
+  </div>
+);
+
+export default BillionareDeleteList;
---
+++ src/pages/DeleteBillionaire/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BillionareDeleteList from '../../components/BillionareDeleteList';
+import style from './style.module.css';
+
+const DeleteBillionare = () => (
+  <div>
+    <BillionareDeleteList />
+  </div>
+);
+
+export default DeleteBillionare;
