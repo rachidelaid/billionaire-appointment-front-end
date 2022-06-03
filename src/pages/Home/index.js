4874a2a7b3d59aaa137a3d568d4fab0ea32736ad
matovu-farid <matovu90@gmail.com>
2022-06-03 04:58:06 +0300

    Add link to details page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import style from './style.module.css'
 import Billionare from '../../components/Billionare';
 const mockData = [
   {
+    "id": 1,
     "name" : "Jeff Bezos",
     "title" : "Amazon CEO",
     "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
@@ -10,6 +11,7 @@ const mockData = [
     "description" : "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.",
   },
   {
+    "id": 2,
     "name" : "Jeff Bezos1",
     "title" : "Amazon CEO",
     "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
@@ -17,6 +19,7 @@ const mockData = [
     "description" : "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.",
   }
   ,{
+    "id": 3,
     "name" : "Jeff Bezos2",
     "title" : "Amazon CEO",
     "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
@@ -32,7 +35,7 @@ const Home = () => (
     </div>
     <div className={style.list}>
       {
-        mockData.map(item=> <Billionare item={item}  key={item.name}/>)
+        mockData.map(item=> <Billionare item={item}  key={item.id}/>)
       }
     </div>
     <button className={style.next}>
