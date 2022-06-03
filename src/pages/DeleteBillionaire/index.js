65c675f007a51f53c834029cec341f3095976379
orozcoding <orozcoding@gmail.com>
2022-06-03 07:35:27 -0400

    Fix linters

--- src/pages/DeleteBillionaire/index.js
+++ src/pages/DeleteBillionaire/index.js
@@ -3,7 +3,7 @@ import BillionaireDeleteList from '../../components/BillionaireDeleteList';
 import style from './style.module.css';
 
 const DeleteBillionaire = () => (
-  <div>
+  <div className={`${style['d-flex']}`}>
     <p>Hello from Delete page</p>
     <BillionaireDeleteList />
   </div>
