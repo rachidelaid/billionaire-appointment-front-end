cae12f0d2ac79a71ed302d4c1f6bf117de990a97
orozcoding <orozcoding@gmail.com>
2022-06-03 04:29:52 -0400

    Render alerts/validation

--- src/components/BillionaireForm/index.js
+++ src/components/BillionaireForm/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './style.module.css';
 
 const apiURL = 'http://localhost:3000/api/billionaires'
 
-
-
 const postBillionaire = async (form) => {
+
+  let result = {
+    response: {},
+    data: {}
+  }
 
   const response = await fetch(apiURL, {
     method: 'POST',
@@ -25,41 +29,81 @@ const postBillionaire = async (form) => {
       image: form.image.value,
       description: form.description.value
     })
-  });
-  return response.json();
+  })
+    .then((resp) => {
+      result.response = resp;
+      return resp.json();
+    })
+    .then((data) => {
+      result.data = data;
+      return data;
+    })
+
+  return result;
 }
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  let form = e.target
-  console.log(form.name.value);
-  await postBillionaire(form)
-  console.log('Redirecting to billionaire details page');
-}
+const BillionaireForm = () => {
 
-const BillionaireForm = () => (
-  <form onSubmit={handleSubmit}>
-    <label>
-      Name:
-      <input type="text" name="name" />
-    </label>
-    <label>
-      Title:
-      <input type="text" name="title" />
-    </label>
-    <label>
-      Price:
-      <input type="number" name="price" />
-    </label>
-    <label>
-      image:
-      <input type="text" name="image" />
-    </label>
-    <label>
-      description:
-      <input type="text" name="description" />
-    </label>
-    <input type="submit" value="Submit" />
-  </form>
-);
+  const [alert, setAlert] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let form = e.target
+    let post = await postBillionaire(form)
+    console.log(post);
+    if (post.response.ok) {
+      console.log('aca llegue');
+      navigate('/')
+      console.log('Redirect to proper route');
+    } else if (!post.response.ok) {
+      console.log(post);
+      let arr = Object.entries(post.data)
+      setAlert(arr);
+      post = null;
+    }
+  }
+
+  const renderAlert = (alert) => {
+
+    return (
+      <div className={`${style['alert-ctn']} ${style['d-flex']} ${style.col}`}>
+        {alert.map((error) => {
+          return <p className={style['alert-item']}>{`${error[0].toUpperCase()} ${error[1]}`}</p>
+        })}
+      </div>
+    )
+
+  }
+
+
+  return (
+    <div className={`${style['d-flex']} ${style.col}`}>
+      {alert ? renderAlert(alert) : null}
+      <form onSubmit={handleSubmit} className={`${style['d-flex']} ${style.col} ${style['form-ctn']}`}>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <label>
+          Price:
+          <input type="number" name="price" step=".01" />
+        </label>
+        <label>
+          image:
+          <input type="text" name="image" />
+        </label>
+        <label>
+          description:
+          <input type="text" name="description" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+};
 export default BillionaireForm;
