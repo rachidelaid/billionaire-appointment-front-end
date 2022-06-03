1a0a0d238cf1ce6722b89855e4fa533537650da7
matovu-farid <matovu90@gmail.com>
2022-06-03 13:34:14 +0300

    Add billionare slice

---
+++ src/redux/billionaires.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const fetchBillionaires = createAsyncThunk('/message/random', async () => {
+  const response = await fetch('http://localhost:3000/api/billionaires');
+  if (response.ok) return response.json();
+  return response.statusText;
+});
+const initialState = {
+  all: []
+};
+const billionaireSlice = createSlice({
+  name: 'billionaires',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchBillionaires.fulfilled, (state, action) => {
+      state.all = action.payload;
+    });
+  },
+});
+export { fetchBillionaires };
+export default billionaireSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import billionareReducer from './billionaires'
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    billionares: billionareReducer
+  },
 });
 
 export default store;
