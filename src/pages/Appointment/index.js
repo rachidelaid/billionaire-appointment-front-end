ee8c50293859302e33066c9c2d528e48f645bb22
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-03 10:48:32 +0100

    Make controlled inputs and local state, with a post fetch that creates an appointment

--- src/pages/Appointment/index.js
+++ src/pages/Appointment/index.js
@@ -1,32 +1,85 @@
+import { useState } from 'react';
 import style from './style.module.css';
 
-const Appointment = () => (
-  <div className={`${style.container} ${style['flex-center']}`}>
-    <div className={`${style.information} ${style['flex-center']}`}>
-      <h1 className={style.heading}>BOOK A BILLIONAIRE</h1>
-      <hr className={style.line} />
-      <p className={style.description}>
-        Ever wanted to spend a day with a billionaire genius who can give you advice
-        on how to run a business?
-        Maybe you just wanted a coffee-chat with him to see the personality behind
-        the money. Well, in Billionaire Appointment this is possible. If you wish
-        to book YOUR billionaire, please use the selector below.
-      </p>
-      <form className={style.form}>
-        <input className={style['form-child']} type="text" placeholder="London" required />
-        <select className={style['form-child']} required>
-          <option value="" selected disabled>Billionaires List</option>
-          <option value="ariel-camus">Ariel Camus</option>
-          <option value="bill-gates">Bill Gates</option>
-          <option value="francoise-meyers">Francoise Meyers</option>
-          <option value="jack-ma">Jack Ma</option>
-        </select>
-        <input type="date" className={style['form-child']} required />
-        <button className={style.btn} type="submit">Book Now</button>
-      </form>
-    </div>
-  </div>
+const Appointment = () => {
+  const [city, setCity] = useState('');
+  const [date, setDate] = useState('');
+  const [billionaire_id, setBillionaire_id] = useState('');
 
-);
+  const createAppointment = (e) => {
+    e.preventDefault();
+
+    const appointment = {
+      city, date, billionaire_id, user_id: 1,
+    };
+
+    fetch('http://localhost:3000/api/appointments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ appointment }),
+    });
+  };
+
+  return (
+    <div className={`${style.container} ${style['flex-center']}`}>
+      <div className={`${style.information} ${style['flex-center']}`}>
+        <h1 className={style.heading}>BOOK A BILLIONAIRE</h1>
+        <hr className={style.line} />
+        <p className={style.description}>
+          Ever wanted to spend a day with a billionaire genius who can give you advice
+          on how to run a business?
+          Maybe you just wanted a coffee-chat with him to see the personality behind
+          the money. Well, in Billionaire Appointment this is possible. If you wish
+          to book YOUR billionaire, please use the selector below.
+        </p>
+        <form className={style.form} onSubmit={(e) => createAppointment(e)}>
+          <input
+            className={style['form-child']}
+            type="text"
+            placeholder="London"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+          <select
+            className={style['form-child']}
+            value={billionaire_id}
+            onChange={(e) => setBillionaire_id(e.target.value)}
+            required
+          >
+            <option value="" disabled>
+              Billionaires List
+            </option>
+            <option value="1">
+              Ariel Camus
+            </option>
+            <option value="2">
+              Bill Gates
+            </option>
+            <option value="3">
+              Francoise Meyers
+            </option>
+            <option value="4">
+              Jack Ma
+            </option>
+          </select>
+          <input
+            className={style['form-child']}
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+          <button
+            className={style.btn}
+            type="submit"
+          >
+            Book Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Appointment;
