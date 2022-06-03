09320702b64250f65316cdbc3ae6dd4c79d9ec2d
matovu-farid <matovu90@gmail.com>
2022-06-03 08:09:08 +0300

    Remove mockdata from index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,7 @@
 import React from 'react';
 import style from './style.module.css'
 import Billionare from '../../components/Billionare';
-const mockData = [
-  {
-    "id": 1,
-    "name" : "Jeff Bezos",
-    "title" : "Amazon CEO",
-    "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
-    "price" : 8000,
-    "description" : "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.",
-  },
-  {
-    "id": 2,
-    "name" : "Jeff Bezos1",
-    "title" : "Amazon CEO",
-    "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
-    "price" : 8000,
-    "description" : "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.",
-  }
-  ,{
-    "id": 3,
-    "name" : "Jeff Bezos2",
-    "title" : "Amazon CEO",
-    "image" : "https://github.com/orozCoding/billionares-pictures/blob/main/round_pictures/pc_jeff.png?raw=true",
-    "price" : 8000,
-    "description" : "Jeff Bezos is an American entrepreneur, media proprietor, investor, computer engineer, and commercial astronaut. He is the founder, executive chairman and former president and CEO of Amazon. With a net worth of around US$140 billion as of May 2022, Bezos is the second-wealthiest person in the world and was the wealthiest from 2017 to 2021 according to both Bloomberg's Billionaires Index and Forbes.",
-  },
-]
+import mockData from '../../mockdata';
 const Home = () => (
   <div className={style.container}>
     <div>
