7605329eb47b3aeb75c0bdf8b13b9a5da7115d77
matovu-farid <matovu90@gmail.com>
2022-06-05 11:59:09 +0300

    Fix: linter errors

--- src/tests/billionaire.test.js
+++ src/tests/billionaire.test.js
@@ -1,29 +1,27 @@
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Wrapper from './helpers/test_wrapper'
-import Billionare from '../components/Billionare'
-
-describe ('billionaire',()=>{
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Wrapper from './helpers/test_wrapper';
+import Billionare from '../components/Billionare';
+/* eslint-disable no-unused-expressions */
+describe('billionaire', () => {
   const item = {
     id: 1,
     name: 'name',
     title: 'title',
     username: 'username',
     price: 0,
-    image: ''
-  }
-  const billionaire = <Wrapper><Billionare item={item} /></Wrapper>
-  beforeEach(()=>{
-
-    render(billionaire) 
-  })
-  it ('should have a title',()=>{
-    const textNode = screen.getByText('title')
-    expect(textNode).toBeInTheDocument
-  })
-  it ('should have a name',()=>{
-    const textNode = screen.getByText('name')
-    expect(textNode).toBeInTheDocument
-  })
-
-})
+    image: '',
+  };
+  const billionaire = <Wrapper><Billionare item={item} /></Wrapper>;
+  beforeEach(() => {
+    render(billionaire);
+  });
+  it('should have a title', () => {
+    const textNode = screen.getByText('title');
+    expect(textNode).toBeInTheDocument;
+  });
+  it('should have a name', () => {
+    const textNode = screen.getByText('name');
+    expect(textNode).toBeInTheDocument;
+  });
+});
