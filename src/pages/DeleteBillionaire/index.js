97c166cf7ffc93832508bc8c0b2f1a0e0444f75b
orozcoding <orozcoding@gmail.com>
2022-06-06 15:54:05 -0400

    Pass user as props from delete page to component

--- src/pages/DeleteBillionaire/index.js
+++ src/pages/DeleteBillionaire/index.js
@@ -14,7 +14,7 @@ const DeleteBillionaire = () => {
         );
       }
       return (
-        <BillionaireDeleteList />
+        <BillionaireDeleteList user={user} />
       );
     }
     return (
