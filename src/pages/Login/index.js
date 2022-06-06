fa681bc8c92a8520e86a707b5ee65b03c5347a86
rachidelaid <rachidelaid@gmail.com>
2022-06-06 17:03:35 +0100

    login logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import style from './style.module.css';
+
+import { login } from '../../redux/users';
 
 const Login = () => {
   const { user } = useSelector((state) => state.users);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (user && user.id) {
@@ -13,19 +16,37 @@ const Login = () => {
     }
   }, [user]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const {
+      email, password,
+    } = e.target.elements;
+
+    const user = {
+      grant_type: 'password',
+      email: email.value.trim(),
+      password: password.value.trim(),
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      client_secret: process.env.REACT_APP_CLIENT_SECRET,
+    };
+
+    dispatch(login(user));
+  };
+
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
-        <div className={style.card}>
+        <form className={style.card} onSubmit={handleSubmit}>
           <h1>Login</h1>
           {(user && user.error)
             && (user.error.map((err) => <p key={err.length} className={style.error}>{err}</p>))}
 
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input type="email" id="email" placeholder="Email" required />
+          <input type="password" id="password" placeholder="Password" required />
 
-          <button type="button" className={style.btn}>Sign In</button>
-        </div>
+          <button type="submit" className={style.btn}>Sign In</button>
+        </form>
         <small className={style.option}>
           Don&rsquo;t have an account?
           {' '}
--- src/redux/users.js
+++ src/redux/users.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+const apiUrl = 'http://localhost:3000';
+
 const setCookie = (token) => {
   document.cookie = `refresh_token=${token}; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure`;
 };
 
 const signup = createAsyncThunk('users/signup', async (userObject) => {
-  const resp = await fetch('http://localhost:3000/api/users', {
+  const resp = await fetch(`${apiUrl}/api/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userObject),
+  });
+  const data = await resp.json();
+
+  return data;
+});
+
+const login = createAsyncThunk('users/login', async (userObject) => {
+  const resp = await fetch(`${apiUrl}/oauth/token`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -36,8 +51,17 @@ const userSlice = createSlice({
         state.user = action.payload;
       }
     });
+
+    builder.addCase(login.fulfilled, (state, action) => {
+      if (action.payload.user) {
+        setCookie(action.payload.user.refresh_token);
+        state.user = { ...action.payload.user, ...action.payload.token };
+      } else {
+        state.user = action.payload;
+      }
+    });
   },
 });
 
-export { signup };
+export { signup, login };
 export default userSlice.reducer;
