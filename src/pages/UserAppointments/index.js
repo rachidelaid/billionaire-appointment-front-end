99acd2e24f16508f86cc43368b1bb2146a758fa9
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-08 15:19:58 +0100

    User appointments are now displayed dinamically

--- src/pages/UserAppointments/index.js
+++ src/pages/UserAppointments/index.js
@@ -2,10 +2,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import style from './style.module.css';
 import { fetchAppointments } from '../../redux/appointments';
+import billionaires, { readableTime } from './billionairesMock';
 
 const UserAppointments = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.users);
+  const appointments = useSelector((state) => state.appointments.all);
+  // const billionaires = useSelector((state) => state.billionaires.all);
 
   useEffect(() => {
     if (user) {
@@ -18,6 +21,16 @@ const UserAppointments = () => {
       <header className={style.header}>
         <h1>Your Appointments</h1>
       </header>
+      <div>
+        {appointments.map((appointment) => (
+          <div key={appointment.id}>
+            <img src={billionaires[appointment.billionaire_id - 1].image} alt={`${billionaires[appointment.billionaire_id - 1].name} logo`} />
+            <p>{appointment.city}</p>
+            <p>{readableTime(appointment.created_at)}</p>
+            <button type="button">Cancel</button>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
